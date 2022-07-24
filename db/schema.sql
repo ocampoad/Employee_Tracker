3a7e25e3921daa2f6d9d45a7d18229d5e6aebff7
ocampoad <ocampo.adriane13@gmail.com>
2022-07-23 20:02:21 -0700

    created connection.js

--- db/schema.sql
+++
@@ -1,6 +0,0 @@
-DROP DATABASE IF EXISTS department_db;
-DROP DATABASE IF EXISTS role_db;
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE department_db;
-CREATE DATABASE role_db;
-CREATE DATABASE employee_db;
