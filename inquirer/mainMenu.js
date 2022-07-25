5733c129bd4d51fe47e134184c6e8178ce2096bb
ocampoad <ocampo.adriane13@gmail.com>
2022-07-24 17:20:53 -0700

    view all employees

--- inquirer/mainMenu.js
+++ inquirer/mainMenu.js
@@ -6,7 +6,7 @@ const connection = require('./../db/connection')
 const mainMenu = () => {
     inquirer.prompt([
         {
-            type: 'list', 
+            type: 'list',
             name: 'menu',
             message: 'What would you like to do?',
             choices: [
@@ -22,40 +22,51 @@ const mainMenu = () => {
     ]).then(async answers => {
         let menuChoice = answers.menu;
         switch (menuChoice) {
-        case 'View all departments':
-            try {
-                let [result] = await connection.query(`SELECT * FROM department`); 
-                console.table("", result)
-            } catch (error) {
-                console.log(error);
-            }
-            break;
-        case 'View all roles':
-            try {
-                let [result] = await connection.query(`
+            case 'View all departments':
+                try {
+                    let [result] = await connection.query(`SELECT * FROM department`);
+                    console.table("", result)
+                } catch (error) {
+                    console.log(error);
+                }
+                break;
+            case 'View all roles':
+                try {
+                    let [result] = await connection.query(`
                 SELECT role.id, role.title, department.name AS department, role.salary
                 FROM role
                 LEFT JOIN department ON role.department_id = department.id;`)
-                console.table("", result)
-            } catch (error) {
-                console.log(error);
-            }
-            break;
-        case 'View all employees':
-            console.log('3');
-            break
-        case 'Add a department':
-            console.log('4');
-            break
-        case 'Add a role':
-            console.log('5')
-            break
-        case 'Add an employee':
-            console.log('6')
-            break
-        case 'Update an employee role':
-            console.log('7');
-            break
+                    console.table("", result)
+                } catch (error) {
+                    console.log(error);
+                }
+                break;
+            case 'View all employees':
+                try {
+                    let [result] = await connection.query(`
+                SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.salary, employee.manager_id AS manager
+                FROM employee
+                INNER JOIN role ON employee.role_id = role.id
+                INNER JOIN department ON role.department_id = department.id
+                ORDER BY employee.id;
+                `)
+                    console.table("", result)
+                } catch (error) {
+                    console.log(error);
+                }
+                break
+            case 'Add a department':
+                console.log('4');
+                break
+            case 'Add a role':
+                console.log('5')
+                break
+            case 'Add an employee':
+                console.log('6')
+                break
+            case 'Update an employee role':
+                console.log('7');
+                break
         };
         mainMenu();
     })
--- model/seeds.sql
+++ model/seeds.sql
@@ -16,3 +16,28 @@ VALUES ('Sales Lead', 100000, 4),
 ('Legal Team Lead', 250000, 3),
 ('Lawyer', 190000, 3);
 
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('John', 'Doe', 1 ),
+('Mike', 'Chan', 2),
+('Ashley', 'Rodriguez', 3),
+('Kevin', 'Tupik', 4),
+('Kunal', 'Singh', 5),
+('Malia', 'Brown', 6),
+('Sarah', 'Lourd', 7),
+('Tom', 'Allen', 8);
+
+UPDATE employee
+SET manager_id = 1
+WHERE first_name = 'Mike';
+
+UPDATE employee
+SET manager_id = 3
+WHERE first_name = 'Kevin';
+
+UPDATE employee
+SET manager_id = 5
+WHERE first_name = 'Malia';
+
+UPDATE employee
+SET manager_id = 7
+WHERE first_name = 'Tom';
